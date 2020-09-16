9a9ff18eb4dfb2c874dd10c743b70795d566c0c2
ramon.jara <ramon.jara@concatel.com>
2020-09-16 16:28:21 +0200

    liquibase refactor, add code to dilution and raw material

--- src/main/resources/liquibase/qa/v1.0.1/ddl/createRoles.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
---changeset efragance:create-table-ROLES-v1.0.1
-DROP TABLE IF EXISTS `ROLES`;
-CREATE TABLE `ROLES` (
-  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `ROLE_NAME` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
