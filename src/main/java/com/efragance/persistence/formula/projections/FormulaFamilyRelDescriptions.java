9ff69f59745e7b0a71efa2d2c2323368a3352dc3
ramon.jara <ramon.jara@concatel.com>
2020-10-02 11:30:55 +0200

    add family and sub families

---
+++ src/main/java/com/efragance/persistence/formula/projections/FormulaFamilyRelDescriptions.java
@@ -0,0 +1,22 @@
+package com.efragance.persistence.formula.projections;
+
+/**
+ * The Interface FormulaFamilyRelDescriptions.
+ */
+public interface FormulaFamilyRelDescriptions {
+
+    /**
+     * Gets the family code.
+     *
+     * @return the family code
+     */
+    String getFamilyCode();
+
+    /**
+     * Gets the sub family code.
+     *
+     * @return the sub family code
+     */
+    String getSubFamilyCode();
+
+}
---
+++ src/main/java/com/efragance/persistence/formula/repositories/FormulaFamilyRelRepository.java
@@ -0,0 +1,34 @@
+package com.efragance.persistence.formula.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
+
+import com.efragance.arch.persistence.repositories.interfaces.BaseRepository;
+import com.efragance.persistence.formula.models.FormulaFamilyRel;
+import com.efragance.persistence.formula.projections.FormulaFamilyRelDescriptions;
+
+/**
+ * The Interface FormulaFamilyRelRepository.
+ */
+public interface FormulaFamilyRelRepository
+        extends BaseRepository<FormulaFamilyRel, Long>, QuerydslPredicateExecutor<FormulaFamilyRel> {
+
+    /**
+     * Gets the families descriptions.
+     *
+     * @param formulaId the formula id
+     * @return the families descriptions
+     */
+    // @formatter:off
+    @Query(value= "SELECT ff.code as familyCode, subff.code as subFamilyCode "
+                + "FROM FormulaFamilyRel ffrel "
+                + "INNER JOIN FormulaFamily ff ON ff.id = ffrel.familyId "
+                + "INNER JOIN FormulaSubFamily subff on subff.id = ffrel.subFamilyId "
+                + "WHERE ffrel.id = :formulaId")
+    // @formatter:on
+    List<FormulaFamilyRelDescriptions> getFamiliesDescriptions(@Param(value = "formulaId") Long formulaId);
+
+}
