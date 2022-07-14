c6706997e7f2af8089f9f458d3352f8daed8b5c7
Jose Ariel Veloso <javeloso@eurofragance.com>
2022-07-14 13:07:11 +0200

    changes upload files

--- src/main/java/com/efragance/persistence/repositories/IngredientTestRepository.java
+++ src/main/java/com/efragance/persistence/repositories/IngredientTestRepository.java
@@ -1,23 +1,16 @@
-package com.efragance.persistence.repositories;
-
-import com.efragance.arch.persistence.repositories.interfaces.BaseRepository;
-import com.efragance.persistence.models.DilutionTest;
-import com.efragance.persistence.models.IngredientTest;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-
-@Repository
-public interface IngredientTestRepository extends BaseRepository<IngredientTest, Long> , QuerydslPredicateExecutor<IngredientTest> {
-
-    @Query("from DilutionTest")
-    List<DilutionTest> findAllDilutions();
-
-
-
-
-
-}
+//package com.efragance.persistence.repositories;
+//
+//import com.efragance.arch.persistence.repositories.interfaces.BaseRepository;
+//
+//import com.efragance.persistence.models.IngredientTest;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//
+//@Repository
+//public interface IngredientTestRepository
+//		extends BaseRepository<IngredientTest, Long>, QuerydslPredicateExecutor<IngredientTest> {
+//
+//}
