0ec87d8161a002493bec2bfc71952ffc477f84fb
ARTURO\artur <arthureloy@gmail.com>
2022-10-05 00:04:22 +0200

    import files chemstatios and amdis

--- src/main/java/com/efragance/persistence/repositories/FormulaAmdisFileRepository.java
+++ src/main/java/com/efragance/persistence/repositories/FormulaAmdisFileRepository.java
@@ -2,9 +2,16 @@ package com.efragance.persistence.repositories;
 
 import com.efragance.arch.persistence.repositories.interfaces.BaseRepository;
 import com.efragance.persistence.models.FormulaAmdisFile;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FormulaAmdisFileRepository extends BaseRepository<FormulaAmdisFile, Long>, QuerydslPredicateExecutor<FormulaAmdisFile> {
+
+	Optional<FormulaAmdisFile> findByFormulaIdAndMethodTableIdAndLibraryIdAndRt(Long formulaId, Long methodTableId, Long libraryId, BigDecimal rt);
+	
 }
--- src/main/java/com/efragance/persistence/repositories/FormulaChemFidFileRepository.java
+++ src/main/java/com/efragance/persistence/repositories/FormulaChemFidFileRepository.java
@@ -3,9 +3,15 @@ package com.efragance.persistence.repositories;
 import com.efragance.arch.persistence.repositories.interfaces.BaseRepository;
 import org.springframework.stereotype.Repository;
 import com.efragance.persistence.models.FormulaChemFidFile;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 @Repository
 public interface FormulaChemFidFileRepository extends BaseRepository<FormulaChemFidFile, Long>, QuerydslPredicateExecutor<FormulaChemFidFile> {
+
+	Optional<FormulaChemFidFile> findByFormulaIdAndMethodTableIdAndRt(Long formulaId, Long methodTableId, BigDecimal stringToBigDecimal);
 
 }
