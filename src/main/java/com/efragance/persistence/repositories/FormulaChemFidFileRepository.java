e809d0d08658286ef34dfeca21346033aa0e6b34
Arturo Pozo <arturo.pozo@cronosforce.com>
2022-10-05 11:54:54 +0000

    Har 130 analysts back apozo

--- src/main/java/com/efragance/persistence/repositories/FormulaChemFidFileRepository.java
+++ src/main/java/com/efragance/persistence/repositories/FormulaChemFidFileRepository.java
@@ -3,13 +3,17 @@ package com.efragance.persistence.repositories;
 import com.efragance.arch.persistence.repositories.interfaces.BaseRepository;
 import org.springframework.stereotype.Repository;
 import com.efragance.persistence.models.FormulaChemFidFile;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 @Repository
 public interface FormulaChemFidFileRepository extends BaseRepository<FormulaChemFidFile, Long>, QuerydslPredicateExecutor<FormulaChemFidFile> {
+
+	Optional<FormulaChemFidFile> findByFormulaIdAndMethodTableIdAndRt(Long formulaId, Long methodTableId, BigDecimal stringToBigDecimal);
 
     List<FormulaChemFidFile> findByFormulaIdAndRtBetween(Long formulaId, BigDecimal before , BigDecimal after);
 
--- src/main/java/com/efragance/persistence/repositories/FormulaChemMSFileRepository.java
+++ src/main/java/com/efragance/persistence/repositories/FormulaChemMSFileRepository.java
@@ -2,9 +2,15 @@ package com.efragance.persistence.repositories;
 
 import com.efragance.arch.persistence.repositories.interfaces.BaseRepository;
 import com.efragance.persistence.models.FormulaChemMsFile;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FormulaChemMSFileRepository extends BaseRepository<FormulaChemMsFile, Long>, QuerydslPredicateExecutor<FormulaChemMsFile> {
+
+	Optional<FormulaChemMsFile> findByFormulaIdAndMethodTableIdAndLibraryIdAndRt(Long formulaId, Long methodTableId, Long libraryId, BigDecimal rt);
 }
