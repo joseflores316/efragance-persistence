68a465e57941251e82cb54d0ee3e8771df333c85
Jose Aylas Flores <jose.aylas@cronosforce.com>
2022-10-06 11:08:37 +0200

    Ordenacion de los formulaCompoundRel

--- src/main/java/com/efragance/persistence/repositories/FormulaCompoundRelRepository.java
+++ src/main/java/com/efragance/persistence/repositories/FormulaCompoundRelRepository.java
@@ -13,11 +13,11 @@ import java.util.Optional;
 @Repository
 public interface FormulaCompoundRelRepository extends BaseRepository<FormulaCompoundRel, Long>, QuerydslPredicateExecutor<FormulaCompoundRel> {
 
-    @Query(value = "select fcr.id, fcr.formulaId, fcr.compoundId, " +
-            "nvl((select fca.rt from FormulaCompArea fca where fca.formulaCompoundRelId = fcr.id and fca.methodTableId = 2), (select fcs.rt from FormulaCompSuggestion fcs where fcs.formulaCompoundRelId = fcr.id and fcs.methodTableId=2)) as rtSIL," +
-            "nvl((select fca.rt from FormulaCompArea fca where fca.formulaCompoundRelId = fcr.id and fca.methodTableId = 1), (select fcs.rt from FormulaCompSuggestion fcs where fcs.formulaCompoundRelId = fcr.id and fcs.methodTableId=1)) as rtCBW" +
-            " from FormulaCompoundRel fcr")
-    List<FormulaCompoundRelDto> findDTOByFormulaIdAndCompoundId(Long formulaId, Long compoundId);
+//    @Query(value = "select fcr.id, fcr.formulaId, fcr.compoundId, " +
+//            "nvl((select fca.rt from FormulaCompArea fca where fca.formulaCompoundRelId = fcr.id and fca.methodTableId = 2), (select fcs.rt from FormulaCompSuggestion fcs where fcs.formulaCompoundRelId = fcr.id and fcs.methodTableId=2)) as rtSIL," +
+//            "nvl((select fca.rt from FormulaCompArea fca where fca.formulaCompoundRelId = fcr.id and fca.methodTableId = 1), (select fcs.rt from FormulaCompSuggestion fcs where fcs.formulaCompoundRelId = fcr.id and fcs.methodTableId=1)) as rtCBW" +
+//            " from FormulaCompoundRel fcr")
+//    List<FormulaCompoundRelDto> findDTOByFormulaIdAndCompoundId(Long formulaId, Long compoundId);
 
 
 //    @Query(value = "select fcr.id, fcr.FORMULA_ID, fcr.COMPOUND_ID, " +
